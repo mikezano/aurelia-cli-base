36e9421d556d57a376eea7122db72af5a42cbaba
mikezano <mikezano@gmail.com>
2017-04-13 19:52:40 -0700

    Finally got tabs working nicely, centered example boxes

--- src/resources/elements/componentElement.ts
+++ src/resources/elements/componentElement.ts
@@ -5,15 +5,22 @@ export class ComponentElement {
 	@bindable file:string;
 	public humanizeFile:string;
 	public scss:string;
+	public pug:string;
+	public scssId:string = this.randomString(16);
+	public pugId:string =  this.randomString(16);
+	public nameId:string =  this.randomString(16);
 
 	public attached(){
 		this.humanize();
-		this.getScss("GET", `src/components/${this.file}.scss`, (result)=>{
+		this.getFile("GET", `src/components/${this.file}.scss`, (result)=>{
 			this.scss = result;
-		})
+		});
+		this.getFile("GET", `src/components/${this.file}.pug`, (result)=>{
+			this.pug = result;
+		})		
 	}
 
-	public getScss (method, url, done) {
+	public getFile (method, url, done) {
 		var xhr = new XMLHttpRequest();
 		xhr.open(method, url);
 		xhr.onload = function () {
@@ -31,4 +38,12 @@ export class ComponentElement {
 			.join(' ');
 	}
 
+	private randomString(length):string {
+		var text = "";
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		for(var i = 0; i < length; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	}
 }
